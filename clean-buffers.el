ed4260a32fa0b2cef9cee98dc3c49e4834a12041
lujun9972 <lujun9972@gmail.com>
2015-12-24 19:32:58 +0800

    rename functions and variables

--- clean-buffers.el
+++ clean-buffers.el
@@ -36,37 +36,37 @@
 
 ;; config `useless-buffer-names' or `useless-buffer-time-out' and then
 ;; execute the following commands:
-;; `cb-kill-useless-buffers' to clean useless buffers
-;; or `cb-turn-on-auto-clean-buffers' to clean useless buffers automatically
+;; `clean-buffer-kill-useless-buffers' to clean useless buffers
+;; or `clean-buffer-turn-on-auto-clean-buffers' to clean useless buffers automatically
 
 ;;; Code:
 
 (require 'cl-lib)
 
-(defun cb--buffer-active-p(buffer)
+(defun clean-buffer--buffer-active-p(buffer)
   "is the BUFFER already show in some window"
   (get-buffer-window buffer t))
 
-(defun cb--buffer-process-holding-p (buffer)
+(defun clean-buffer--buffer-process-holding-p (buffer)
   "is the BUFFER holding a process"
   (get-buffer-process buffer))
 
 (defgroup clean-buffers nil
   "clean useless buffers"
-  :prefix "cb-"
+  :prefix "clean-buffer-"
   :group 'convenience)
 
-(defcustom cb-kill-active-buffer nil
+(defcustom clean-buffer-kill-active-buffer nil
   "clean active buffer or not"
   :type '(boolean)
   :group 'clean-buffers)
 
-(defcustom cb-kill-proces-holding-buffer nil
+(defcustom clean-buffer-kill-proces-holding-buffer nil
   "clean process-holding buffer or not"
   :type '(boolean)
   :group 'clean-buffers)
 
-(defcustom cb-judge-useless-buffer-functions '(cb-judge-useless-buffer-by-time cb-judge-useless-buffer-by-name)
+(defcustom clean-buffer-judge-useless-buffer-functions '(clean-buffer-judge-useless-buffer-by-time clean-buffer-judge-useless-buffer-by-name)
   "function list which used to determine a buffer is useless or not 
 
 the function will take a buffer as the only argument and should return non-nil when the buffer is a useless buffer."
@@ -74,75 +74,75 @@ the function will take a buffer as the only argument and should return non-nil w
   :group 'clean-buffers
   :type '(repeat function))
 
-(defcustom cb-useless-buffer-time-out (* 7 24 3600)
-  "used in `cb-judge-useless-buffer-by-time'"
+(defcustom clean-buffer-useless-buffer-time-out (* 7 24 3600)
+  "used in `clean-buffer-judge-useless-buffer-by-time'"
   :group 'clean-buffers
   :type '(integer))
 
-(defun cb-judge-useless-buffer-by-time (buffer)
+(defun clean-buffer-judge-useless-buffer-by-time (buffer)
   "buffer which did not displayed for specify time considered to be useless
 
-the expire time is determined by `cb-useless-buffer-time-out'"
+the expire time is determined by `clean-buffer-useless-buffer-time-out'"
   (let (now buffer-last-display-time)
 	(setq now (float-time (current-time)))
 	(setq buffer-last-display-time (float-time (buffer-local-value 'buffer-display-time (get-buffer buffer))))
-	(> (- now buffer-last-display-time) cb-useless-buffer-time-out)))
+	(> (- now buffer-last-display-time) clean-buffer-useless-buffer-time-out)))
 
-(defcustom cb-useless-buffer-names 
+(defcustom clean-buffer-useless-buffer-names 
 	'("*Buffer List*" "*Backtrace*" "*Apropos*" "*Completions*" "*Help*" "\\.~master~" "\\*vc-dir\\*" "\\*tramp\/.+\\*"  "\\*vc-git.+\\*")
 	"useless buffer list"
 	:group 'clean-buffers
 	:type '(repeat regexp))
 
-(defun cb-judge-useless-buffer-by-name (buffer)
+(defun clean-buffer-judge-useless-buffer-by-name (buffer)
   ""
-  (cl-some (lambda (reg) (string-match reg buffer)) cb-useless-buffer-names))
+  (cl-some (lambda (reg) (string-match reg buffer)) clean-buffer-useless-buffer-names))
 
-(defcustom cb-useful-buffer-names 
+(defcustom clean-buffer-useful-buffer-names 
 	'("*Tree*")
 	"useful buffer list"
 	:group 'clean-buffers
 	:type '(repeat regexp))
 
-(defun cb--useless-buffer-p (buffer)
-  "use functions in `cb-judge-useless-buffer-functions' to determine the BUFFER is a useless buffer or not"
+(defun clean-buffer--useless-buffer-p (buffer)
+  "use functions in `clean-buffer-judge-useless-buffer-functions' to determine the BUFFER is a useless buffer or not"
   (when (bufferp buffer)
 	(setq buffer (buffer-name buffer)))
-  (and (not (cl-some (lambda (reg) (string-match reg buffer)) cb-useful-buffer-names))
-	   (cl-some (lambda (fn) (funcall fn buffer)) cb-judge-useless-buffer-functions)))
+  (and (not (cl-some (lambda (reg) (string-match reg buffer)) clean-buffer-useful-buffer-names))
+	   (cl-some (lambda (fn) (funcall fn buffer)) clean-buffer-judge-useless-buffer-functions)))
 
-(defun cb--kill-useless-buffer(buffer &optional kill-active kill-process-holding)
+(defun clean-buffer--kill-useless-buffer(buffer &optional kill-active kill-process-holding)
   "kill the BUFFER if the BUFFER is a useless buffer"
-  (unless (or (not (cb--useless-buffer-p buffer))
-			  (and (not kill-active) (cb--buffer-active-p buffer))
-			  (and (not kill-process-holding) (cb--buffer-process-holding-p buffer)))
+  (unless (or (not (clean-buffer--useless-buffer-p buffer))
+			  (and (not kill-active) (clean-buffer--buffer-active-p buffer))
+			  (and (not kill-process-holding) (clean-buffer--buffer-process-holding-p buffer)))
 	(kill-buffer buffer)))
 
 ;;;###autoload
-(defun cb-kill-useless-buffers()
+(defun clean-buffer-kill-useless-buffers()
   "clean all useless buffer"
   (interactive)
   (dolist (buffer (buffer-list))
-	(cb--kill-useless-buffer buffer cb-kill-active-buffer cb-kill-proces-holding-buffer)))
+	(clean-buffer--kill-useless-buffer buffer clean-buffer-kill-active-buffer clean-buffer-kill-proces-holding-buffer)))
 
-(defcustom cb-auto-clean-interval 10
+(defcustom clean-buffer-auto-clean-interval 10
   "clean useless buffers interval"
   :type '(integer)
   :group 'clean-buffers)
 
-(defvar cb-auto-clean-timer nil)
+(defvar clean-buffer-auto-clean-timer nil)
 
 ;;;###autoload
-(defun cb-turn-off-auto-clean-buffers ()
+(defun clean-buffer-turn-off-auto-clean-buffers ()
   (interactive)
-  (when (timerp cb-auto-clean-timer)
-    (cancel-timer cb-auto-clean-timer)))
+  (when (timerp clean-buffer-auto-clean-timer)
+    (cancel-timer clean-buffer-auto-clean-timer)))
 
 ;;;###autoload
-(defun cb-turn-on-auto-clean-buffers ()
+(defun clean-buffer-turn-on-auto-clean-buffers ()
   (interactive)
-  (cb-turn-off-auto-clean-buffers)
-  (setq cb-auto-clean-timer (run-with-timer 0 cb-auto-clean-interval #'cb-kill-useless-buffers)))
+  (clean-buffer-turn-off-auto-clean-buffers)
+  (setq clean-buffer-auto-clean-timer (run-with-timer 0 clean-buffer-auto-clean-interval #'clean-buffer-kill-useless-buffers)))
 
 (provide 'clean-buffers)
 
